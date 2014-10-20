41eadcc67369a061658decf4dfb690cb769f3a0a
r0430551 <r0430551@179679de-6aac-43da-8a4f-fd283e129c2a>
2014-10-20 03:18:30 +0000

    Stukje documentatie. Maar nu ga'k mn bed in.

--- website/src/database/UserDatabase.java
+++ website/src/database/UserDatabase.java
@@ -7,17 +7,64 @@ import java.util.Collection;
 
 public interface UserDatabase {
 
+    /**
+     * Voegt een gebruiker toe aan de databank en instantieert deze.
+     * 
+     * @param name
+     * @param email
+     * @param password
+     * @param level
+     * @return
+     * @throws DatabaseException
+     */
     public User add(String name, String email, String password, AccesLevel level)
             throws DatabaseException;
     
+    /**
+     * Verwijderd een gebruiker uit de databank
+     * 
+     * @param user
+     * @return
+     * @throws DatabaseException
+     */
     public boolean remove(User user) throws DatabaseException;
 
+    /**
+     * Schrijft eventuele wijzigingen weg naar de databank.
+     * 
+     * @param user
+     * @return
+     * @throws DatabaseException
+     */
     public boolean update(User user) throws DatabaseException;
 
+    /**
+     * Instantieert een gebruiker op basis van zijn Id
+     * 
+     * @param id
+     * @return
+     * @throws DatabaseException
+     */
     public User get(long id) throws DatabaseException;
     
+    /**
+     * Instantieert een gebruiker op basis van zijn e-mail
+     * 
+     * @param email
+     * @return
+     * @throws DatabaseException
+     */
     public User getByEmail(String email) throws DatabaseException;
 
+    /**
+     * Instantieert een gebruiker op basis van de combinatie e-mail en wachtwoord
+     * Gebruikt voor login
+     * 
+     * @param email
+     * @param password
+     * @return
+     * @throws DatabaseException
+     */
     public User getByCredentials(String email, String password) throws DatabaseException;
     
     public Collection<User> getAll();
