1ea8fc1ea306e7e32850fb3e2154aa673e4f2760
r0430551 <r0430551@179679de-6aac-43da-8a4f-fd283e129c2a>
2014-11-29 19:52:43 +0000

    Aanvullen QuestionDatabase interface

--- website/src/java/database/QuestionDatabase.java
+++ website/src/java/database/QuestionDatabase.java
@@ -4,11 +4,18 @@ import domain.Question;
 import domain.User;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface QuestionDatabase {
+    
+    public Question add(User user, String text) throws DatabaseException;
 
     public Question add(User user, String text, boolean approved,
             boolean reviewed, boolean removed) throws DatabaseException;
+    
+    public List<Question> getByUser(User user) throws DatabaseException;
+    
+    public List<Question> getByUser(User user, String text) throws DatabaseException;
 
     public void remove(Question question) throws DatabaseException;
 
