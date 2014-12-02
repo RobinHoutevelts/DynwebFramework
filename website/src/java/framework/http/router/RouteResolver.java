d46718dfc4f9b4367d2f4c9f5b1f68734f730066
r0430551 <r0430551@179679de-6aac-43da-8a4f-fd283e129c2a>
2014-12-02 19:18:08 +0000

    Refactoring Routing

--- website/src/java/framework/http/router/RouteResolver.java
+++ website/src/java/framework/http/router/RouteResolver.java
@@ -3,9 +3,7 @@ package framework.http.router;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 
@@ -18,50 +16,37 @@ import framework.http.Response;
 public class RouteResolver implements Resolver {
 
     private Container app;
-    
-    private String regex;
-    private Class controllerClass;
-    private String methodName;
-    private List<String> parameters;
+    private Route route;
 
-    public RouteResolver(Container app, String regex, Class controller, String methodName, List<String> parameters) {
+    public RouteResolver(Container app, Route route){
         this.app = app;
-        this.regex = regex;
-        this.controllerClass = controller;
-        this.methodName = methodName;
-        this.parameters = parameters;
+        this.route = route;
     }
 
     public void resolve(Request request, Response response) throws ServletException {
 
-        String URI = request.getRequestURI().replaceAll(request.getContextPath(), "");
-
-        Pattern pattern = Pattern.compile(this.regex);
-        Matcher matcher = pattern.matcher(URI);
-
-        int groups = matcher.groupCount();
-
-        int i = 0;
-        if (matcher.matches()) {
-            while (groups > i) {
-                String key = this.parameters.get(i);
-                String val = matcher.group(i + 1);
-
-                request.addParameter(key, val);
-                i++;
-            }
+        String url = request.getRequestURI().replaceAll(request.getContextPath(), "");
+        
+        Class controllerClass = this.route.getControllerClass();
+        String methodName = this.route.getMethodName();
+        
+        HashMap<String, String> parameters = this.route.extractParameters(url);
+        for(String key : parameters.keySet()){
+            String value = parameters.get(key);
+            
+            request.addParameter(key, value);
         }
 
         try {
-            Constructor constructorClass = this.controllerClass.getConstructor(Container.class);
+            Constructor constructorClass = controllerClass.getConstructor(Container.class);
             Controller controller = (Controller) constructorClass.newInstance(this.app);
-            Method method = this.controllerClass.getMethod(this.methodName,Request.class, Response.class);
+            Method method = controllerClass.getMethod(methodName,Request.class, Response.class);
 
             method.invoke(controller, request, response);
         }catch( InstantiationException
                 | IllegalArgumentException | InvocationTargetException
                 | NoSuchMethodException | SecurityException | IllegalAccessException e) {
-            throw new ServletException("Could not resolve Controller '"+ this.controllerClass.getName() + "@" + this.methodName+ "'",e);
+            throw new ServletException("Could not resolve Controller '"+ controllerClass.getName() + "@" + methodName+ "'",e);
         }
         
     }
